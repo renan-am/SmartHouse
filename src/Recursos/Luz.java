aa971436f04559761e4f9765aa048cfbe2c48378
adolfpc <38138418+adolfpc@users.noreply.github.com>
2018-06-25 17:58:02 -0300

    Add files via upload

---
+++ src/Recursos/Luz.java
@@ -0,0 +1,45 @@
+package Recursos;
+
+import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+
+public class Luz extends Objeto implements Serializable{
+	QualidadeLuz intensidade;
+	QualidadeLuz cor;
+	
+	public Luz (QualidadeLuz intensidade, QualidadeLuz cor, String nome, int potencia, String comodo) {
+		super(nome, potencia, comodo);
+		this.intensidade = intensidade;
+		this.cor = cor;
+	}
+	
+	public QualidadeLuz getIntensidade() {
+		return intensidade;
+	}
+	
+	public void setIntensidade(QualidadeLuz intensidade) {
+		this.intensidade = intensidade;
+	}
+	
+	public QualidadeLuz getCor() {
+		return cor;
+	}
+	
+	public void setCor(QualidadeLuz cor) {
+		this.cor = cor;
+	}
+	
+	public String DefinirStatus() {
+		if(super.getEstado() == true) {
+			String out = "A luz esta acesa, com intensidade" +getIntensidade()+ "e cor atual" +getCor();
+			return out;			
+		}
+		else {
+			String out = "Luz apagada";
+			return out;
+		}
+	}
+	
+}
