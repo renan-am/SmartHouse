93a168e6afd5f35a4493bd803ec1b8304c01c48c
Renan <recam94@gmail.com>
2018-06-25 17:24:37 -0300

    interface esqueleto

--- src/Aplicativo/Controller.java
+++ src/Aplicativo/Controller.java
@@ -1,4 +1,12 @@
 package Aplicativo;
 
 public class Controller {
+
+
+
+    public Controller(int a, int b) {
+        
+    }
+
+
 }
--- src/Aplicativo/Main.java
+++ src/Aplicativo/Main.java
@@ -10,15 +10,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Aplicativo.fxml"));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setControllerFactory(c -> {
+            return new Controller(1,2);
+        });
+        Parent root = loader.load(getClass().getResource("Aplicativo.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        //launch(args);
-        System.out.println("Teste");
+        launch(args);
+
+
+
     }
 }
