5166ee20076c7d3a4b021f125c2cb4ebd0f480aa
Júlio John Tavares Ramos <30352999+kan4000@users.noreply.github.com>
2018-06-25 16:49:26 -0300

    Add files via upload

---
+++ src/Recursos/TV.java
@@ -0,0 +1,53 @@
+
+public class TV extends Objeto{
+	private int canal;
+	private int brilho;
+	private int contraste;
+	private int volume;
+	public int getCanal() {
+		return canal;
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	public int getBrilho() {
+		return brilho;
+	}
+	public void setBrilho(int brilho) {
+		this.brilho = brilho;
+	}
+	public int getContraste() {
+		return contraste;
+	}
+	public void setContraste(int contraste) {
+		this.contraste = contraste;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	public TV() //padrao de fabrica
+	{
+		this.canal = 0;
+		this.brilho = 50;
+		this.contraste = 50;
+		this.volume = 50;
+	}
+	
+	public String toString()
+	{
+		String out = "A tv está no canal  " + this.canal + "\n";
+		out += "Brilho: " + this.brilho + " Contraste: " + this.contraste + " Volume: " + this.volume + "\n";
+		return out;
+	}
+	
+	public String definirStatus()
+	{
+		String out = "A tv está no canal  " + this.canal + "\n";
+		out += "Brilho: " + this.brilho + " Contraste: " + this.contraste + " Volume: " + this.volume + "\n";
+		return out;
+	}
+}
