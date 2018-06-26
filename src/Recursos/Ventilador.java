b8153fa05e11367682cd7ed2683b77bfe6d80348
adolfpc <38138418+adolfpc@users.noreply.github.com>
2018-06-25 21:35:50 -0300

    Add files via upload

--- src/Recursos/Ventilador.java
+++ src/Recursos/Ventilador.java
@@ -32,7 +32,7 @@ public class Ventilador extends Objeto implements Serializable{
 	}
 	
 	public String definirStatus() {
-		if(super.isEstado()) {
+		if(super.getEstado() == true) {
 			String out = "Ventilar ligado. Estado de rotacao: ";
 			if(isRotacao())
 				out = out + "Rotacionando.";
