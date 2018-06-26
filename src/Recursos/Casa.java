81afd6e59bb75a57cf1f5f9482eaccd905ea79f9
Júlio John Tavares Ramos <30352999+kan4000@users.noreply.github.com>
2018-06-25 21:46:31 -0300

    Update Casa.java

--- src/Recursos/Casa.java
+++ src/Recursos/Casa.java
@@ -46,6 +46,15 @@ public class Casa implements Serializable{
 		this.numComodos = numComodos;
 	}
 	
+	public Objeto maisUtilizado() {
+		Objeto maiorObjeto = objetos.get(0);
+		for (int i = 0; i < objetos.size - 1; i++) {
+			if(objetos.get(i).getTempoDeUso() > objetos.get(i+1).getTempoDeUso())
+				maiorObjeto = objetos.get(i);
+		}
+		return maiorObjeto;
+	}
+	
 	public double consumoTotal() {
 		double parcial = 0;
 		for (Objeto aux : objetos) {
